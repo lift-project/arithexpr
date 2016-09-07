dfff0e7efffa2ae5e8e0499bb48b5575012e828d
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2016-09-07 10:56:58 +0000

    ArithExpr.scala edited online with Bitbucket

--- src/main/apart/arithmetic/ArithExpr.scala
+++ src/main/apart/arithmetic/ArithExpr.scala
@@ -50,9 +50,7 @@ abstract sealed class ArithExpr {
       case c: Cst => (c, c)
       case Prod(factors) =>
         this.sign match {
-          case Sign.Positive =>
-            println(this)
-            (factors.map(abs(_).min).reduce[ArithExpr](_ * _), factors.map(abs(_).max).reduce[ArithExpr](_ * _))
+          case Sign.Positive => (factors.map(abs(_).min).reduce[ArithExpr](_ * _), factors.map(abs(_).max).reduce[ArithExpr](_ * _))
           case Sign.Negative => (factors.map(abs(_).max).reduce[ArithExpr](_ * _) * -1, factors.map(abs(_).min).reduce[ArithExpr](_ * _) * -1)
           case Sign.Unknown => (?, ?) // impossible to determine the min and max
         }
