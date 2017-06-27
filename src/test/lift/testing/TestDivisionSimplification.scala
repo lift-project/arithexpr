c9447f58a0b3416bf3f9fc1062cad7cbe01d3228
Larisa Stoltzfus <larisa.stoltzfus@ed.ac.uk>
2017-06-27 14:45:50 +0100

    adding test for issue #4

--- src/test/lift/testing/TestDivisionSimplification.scala
+++ src/test/lift/testing/TestDivisionSimplification.scala
@@ -1,6 +1,7 @@
 package lift.testing
 
-import lift.arithmetic.{Cst, Var}
+import lift.arithmetic._
+import opencl.generator.OclFunction
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
@@ -21,5 +22,15 @@ class TestDivisionSimplification {
       c + a /^ (Cst(42) + b),
       (a + Cst(42) * c + b * c) /^ (Cst(42) + b)
     )
+  }
+
+  @Test def issueNumber4(): Unit = {
+    val v_O_0 = Var("v_O_0",StartFromRange(1))
+    val v_gl_id_13 =   OclFunction("get_global_id", 0, ContinuousRange(0, OclFunction("get_global_size", 0, ContinuousRange(1,PosInf))))
+    val inSize  = Cst(3)
+    val i = (((4+(2*v_O_0))) / ((2+v_O_0))+(3*v_gl_id_13))
+
+    i/inSize
+    println("made it !")
   }
 }
