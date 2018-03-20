f826027565241c0a38f34d2bc4a330670b5c930e
Bastian Hagedorn <b.hagedorn@wwu.de>
2018-03-20 15:48:16 +0100

    improve IfThenElse simplification

--- src/main/lift/arithmetic/simplifier/SimplifyIfThenElse.scala
+++ src/main/lift/arithmetic/simplifier/SimplifyIfThenElse.scala
@@ -2,17 +2,46 @@ package lift
 package arithmetic
 package simplifier
 
+import lift.arithmetic.Predicate.Operator
+
 object SimplifyIfThenElse {
 
   def simplify(test: Predicate, t : ArithExpr, e : ArithExpr): Option[ArithExpr] = {
     // If both branches are the same, it doesn't matter which one we take
-    if (e == t) Some(t)
+
+    if (e == t) return Some(t)
+
     // otherwise try to evaluate the predicate
+    val lmin = test.lhs.min
+    val lmax = test.lhs.max
+    val rmin = test.rhs.min
+    val rmax = test.rhs.max
+
+    if(test.op == Operator.<) test.lhs match {
+      case v: Var if ArithExpr.isSmaller(v.range.max, test.rhs).getOrElse(false) => return Some(t)
+      case Sum(Cst(c) :: (v:Var) :: Nil) if(
+        c < 0 && ArithExpr.isSmaller(v.range.max, test.rhs).getOrElse(false)) => return Some(t)
+      case _ =>
+    }
+
+    // >= => min of lhs needs to be always bigger or equals to the max of rhs
+    if(test.op == Operator.>= && (ArithExpr.isSmaller(rmax, lmin).getOrElse(false) || rmax == lmin))
+      return Some(t)
+
+    if(test.op == Operator.<= && (ArithExpr.isSmaller(lmax, rmin).getOrElse(false) || rmin == lmax))
+      return Some(t)
+
+    if(test.op == Operator.> && ArithExpr.isSmaller(rmax, lmin).getOrElse(false))
+      return Some(t)
+
+    if(test.op == Operator.< && ArithExpr.isSmaller(lmax, rmin).getOrElse(false))
+      return Some(t)
+
     else {
-      ExprSimplifier(test.lhs - test.rhs) match {
+      val p = ExprSimplifier(test.lhs - test.rhs)
+      p match {
         case Cst(v) =>
           val op = test.op
-          import lift.arithmetic.Predicate.Operator
           // true predicate
           if ((v > 0 && (op == Operator.> || op == Operator.>=)) ||
             (v < 0 && (op == Operator.< || op == Operator.<=)) ||
