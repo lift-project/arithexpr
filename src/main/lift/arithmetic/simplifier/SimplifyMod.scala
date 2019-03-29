4ee7b737ebacbbbf38873eeee748423c7d806096
Naums Mogers <naums.mogers@gmail.com>
2019-03-29 18:54:17 +0000

    Restrictive matching in SimplifyMod

--- src/main/lift/arithmetic/simplifier/SimplifyMod.scala
+++ src/main/lift/arithmetic/simplifier/SimplifyMod.scala
@@ -163,8 +163,8 @@ object SimplifyMod {
       Some((s - c + c%d) % d)
 
     // Isolate the terms which are multiple of the mod and eliminate
-    case (s@Sum(terms), d) if !ArithExpr.mightBeNegative(s) =>
-      val (multiple, _) = terms.partition(t => (t, d) match {
+    case (s: Sum, d) if !ArithExpr.mightBeNegative(s) =>
+      val (multiple, _) = s.terms.partition(t => (t, d) match {
         case (Prod(factors1), Prod(factors2)) => factors2 forall (factors1 contains)
         case (Prod(factors), x) if factors.contains(x) => true
         case (x, y) if ArithExpr.multipleOf(x, y) => true
