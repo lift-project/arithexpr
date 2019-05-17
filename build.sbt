d008ba98c1b3df716483835732af0a5c78add2a2
Naums <naums.mogers@gmail.com>
2019-05-17 14:21:18 +0100

    Reverting commits that change Scala and its dependency versions to see if Jenkins build will succeed anyway
    This reverts commit 7c70aef1b82b4d43848365ad8d9e58b9e66dd64c.

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-ThisBuild / scalaVersion := "2.12.0"
+ThisBuild / scalaVersion := "2.11.12"
 ThisBuild / organization := "org.lift-project"
 
 lazy val arithExpr = (project in file("."))
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,16 +3,16 @@ import sbt._
 object Dependencies {
     lazy val libraryDependencies = Seq(
         // scala
-        "org.scala-lang" % "scala-reflect" % "2.12.0",
-        "org.scala-lang" % "scala-compiler" % "2.12.0",
-        "org.scala-lang" % "scala-library" % "2.12.0",
+        "org.scala-lang" % "scala-reflect" % "2.11.12",
+        "org.scala-lang" % "scala-compiler" % "2.11.12",
+        "org.scala-lang" % "scala-library" % "2.11.12",
     // testing
         "junit" % "junit" % "4.11",
         "com.novocode" % "junit-interface" % "0.11" % "test",
-        "org.scalacheck" %% "scalacheck" % "1.14.0" % "test",
+        "org.scalacheck" %% "scalacheck" % "1.13.0" % "test",
     // XML
-        "org.scala-lang.modules" % "scala-xml_2.12" % "1.0.4",
+        "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.4",
     // cli parser
-//        "org.clapper" %% "argot" % "1.0.3"
+        "org.clapper" %% "argot" % "1.0.3"
     )
 }
