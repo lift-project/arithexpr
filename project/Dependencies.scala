af790426c1f0b2fb1b3a00a4bd36841928cfdbf8
Naums <naums.mogers@gmail.com>
2019-06-10 21:16:33 +0100

    Reverting Scala version to the original

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object Dependencies {
     lazy val libraryDependencies = Seq(
         // scala
-        "org.scala-lang" % "scala-reflect" % "2.11.8",
-        "org.scala-lang" % "scala-compiler" % "2.11.8",
-        "org.scala-lang" % "scala-library" % "2.11.8",
+        "org.scala-lang" % "scala-reflect" % "2.11.12",
+        "org.scala-lang" % "scala-compiler" % "2.11.12",
+        "org.scala-lang" % "scala-library" % "2.11.12",
     // testing
         "junit" % "junit" % "4.11",
         "com.novocode" % "junit-interface" % "0.11" % "test",
-        "org.scalacheck" %% "scalacheck" % "1.14.0" % "test",
+        "org.scalacheck" %% "scalacheck" % "1.13.0" % "test",
     // XML
         "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.4",
     )
