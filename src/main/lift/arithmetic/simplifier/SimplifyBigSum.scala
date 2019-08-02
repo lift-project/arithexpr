56ea5bac2d9df5790ed650ef4fe73e2ba98c2476
federico <pizzutifederico27@gmail.com>
2019-08-02 15:16:11 +0100

    First attempt to implment smarter big sum simplification

--- src/main/lift/arithmetic/simplifier/SimplifyBigSum.scala
+++ src/main/lift/arithmetic/simplifier/SimplifyBigSum.scala
@@ -13,8 +13,11 @@ object SimplifyBigSum {
           ((bigSum.upTo + 1) - bigSum.from)*bigSum.body
           //Split the sum along +
         case Sum(terms) if terms.nonEmpty =>
-          val sumTerms = terms.map(term => bigSum.modify(newBody = term))
-          sumTerms.reduceOption(_ + _).getOrElse(Cst(0))
+
+          val SumTermSimplificationResult(processedTerms, toTakeOut) = simplifySumTerms(bigSum.variable, bigSum.upTo, terms)
+
+          val sumTerms = processedTerms.map(term => bigSum.modify(newBody = term))
+          (sumTerms ++ toTakeOut).reduceOption(_ + _).getOrElse(Cst(0))
           //Take out constant products
         case Prod(factors) if factors.nonEmpty =>
           val (keepIn, takeOut) = factors.partition(factor => ArithExpr.freeVariables(factor).contains(bigSum.variable))
@@ -49,4 +52,61 @@ object SimplifyBigSum {
         case _ => done(bigSum)
       }
   }
+
+  private case class SumTermSimplificationResult(keepInTheSum:Seq[ArithExpr with SimplifiedExpr], takeOutOfSum:Seq[ArithExpr with SimplifiedExpr])
+
+  private def simplifySumTerms(iterVar:Var,
+                               sumUpperBound:ArithExpr,
+                               exprs: List[ArithExpr with SimplifiedExpr]):SumTermSimplificationResult = {
+    val (functions, notFunctions) = exprs.partition(_.isInstanceOf[ArithExprFunctionCall])
+    val (relevantFunctions, irrelevantFunctions) = functions
+      .map(_.asInstanceOf[ArithExprFunctionCall])
+      .map(functionWithRespectToVar(_, iterVar))
+      .partition(_.isDefined)
+
+    val SumTermSimplificationResult(keepInSum, takeOutOfSum) = findPlusOnePairs(sumUpperBound, relevantFunctions.map(_.get), Seq(), Seq())
+
+    SumTermSimplificationResult(
+      keepInSum ++ notFunctions ++ irrelevantFunctions.map(_.get.originalFunction),
+      takeOutOfSum
+    )
+  }
+
+  private def findPlusOnePairs(sumUpperBound:ArithExpr,
+                               funs:List[FunctionWithRespectToSingleVar],
+                               seen:Seq[FunctionWithRespectToSingleVar],
+                               takenOut:Seq[ArithExpr with SimplifiedExpr]):
+  SumTermSimplificationResult = {
+    funs match {
+      case Nil => SumTermSimplificationResult(seen.map(_.originalFunction), takenOut)
+      case fun::rest =>
+        val sibling = if(fun.arg == fun.v + 1) {
+          seen.find(x => x.arg == fun.v)
+        } else if(fun.arg == fun.v) {
+          seen.find(x => x.arg == 0 - fun.v)
+        } else None
+
+        sibling match {
+          case None => findPlusOnePairs(sumUpperBound, rest, seen :+ fun, takenOut)
+          case Some(matching) =>
+            val newEntries = Seq(fun.replaceArg(sumUpperBound), 0 - fun.replaceArg(0))
+            findPlusOnePairs(sumUpperBound,  rest, seen.filter(_ != matching), takenOut ++ newEntries)
+        }
+    }
+  }
+
+  private def functionWithRespectToVar(fun:ArithExprFunctionCall, v:Var):Option[FunctionWithRespectToSingleVar] = {
+    fun.exposedArgs.filter(ArithExpr.freeVariables(_).contains(v)).toList match {
+      case x::Nil => Some(FunctionWithRespectToSingleVar(fun, v, x))
+      case _ => None
+    }
+  }
+
+  private case class FunctionWithRespectToSingleVar(originalFunction:ArithExprFunctionCall, v:Var, val arg:ArithExpr) {
+    def replaceArg(ae:SimplifiedExpr):ArithExprFunctionCall = {
+      originalFunction.substituteExposedArgs(Map(arg -> ae))
+    }
+  }
+
+
 }
